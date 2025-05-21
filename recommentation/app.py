47b6e3e47c86e578480263174daaa41c3328bee3
rajpatil-2424 <166523517+rajgpatil@users.noreply.github.com>
2025-05-22 00:56:01 +0530

    try-to-solve-deployment-erro

--- recommentation/app.py
+++ recommentation/app.py
@@ -111,5 +111,4 @@ def get_popular_dishes():
     return jsonify({"popular_dishes": popular_products_sorted})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or 5000 as fallback
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", debug=True)
